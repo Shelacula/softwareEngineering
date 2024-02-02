180e7f2d21886530b0dc27fd9d6210de8d5ece31
Shelacula <shelacula@yahoo.com>
2024-02-02 16:27:35 -0500

    modified:   Storage.java

--- Storage.java
+++ Storage.java
@@ -1,11 +1,11 @@
 public void datStorageTest(Storage store){
-    InputReturn inputTest = store.input(new Source());
-    DelimitReturn delimTest = store.delim(new Delim());
-    OutputReturn outputTest = store.output(new Output());
+  InputReturn inputTest = store.input(new Source());
+  DelimitReturn delimTest = store.delim(new Delim());
+  OutputReturn outputTest = store.output(new Output());
 }
 
 public Interface Storage{
-    InputReturn input();
-    DelimitReturn delim();
-    OutputReturn output();
+  InputReturn input();
+  DelimitReturn delim();
+  OutputReturn output();
 }
