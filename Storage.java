e9fc49f5df066ebeb87f782f3c7adf42a7fec7d3
Shelacula <40150200+Shelacula@users.noreply.github.com>
2024-02-02 16:45:23 -0500

    modified:   Storage.java (#3)
    * modified:   Storage.java
    
    * modified:   Storage.java

--- Storage.java
+++ Storage.java
@@ -1,10 +1,10 @@
-public static void dataStorageTest(Storage store){
+public void dataStorageTest(Storage store){
   InputReturn inputTest = store.input(new Source());
   DelimitReturn delimTest = store.delim(new Delim());
   OutputReturn outputTest = store.output(new Output());
 }
 
-public Interface Storage{
+public interface Storage{
   InputReturn input();
   DelimitReturn delim();
   OutputReturn output();
