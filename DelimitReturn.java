da8eaa77352f3d0b4b05800834d50ff269dc8e74
Shelacula <shelacula@yahoo.com>
2024-02-02 22:18:46 -0500

    new file:   DelimitReturn.java 	new file:   InputReturn.java 	new file:   OutputReturn.java 	modified:   Storage.java

---
+++ DelimitReturn.java
@@ -0,0 +1,3 @@
+public interface DelimitReturn{
+  boolean success();
+}
--- Storage.java
+++ Storage.java
@@ -1,8 +1,8 @@
-public void dataStorageTest(Storage store){
-  InputReturn inputTest = store.input(new Source());
-  DelimitReturn delimTest = store.delim(new Delim());
-  OutputReturn outputTest = store.output(new Output());
-}
+// public void dataStorageTest(Storage store){
+//   InputReturn inputTest = store.input(new Source());
+//   DelimitReturn delimTest = store.delim(new Delim());
+//   OutputReturn outputTest = store.output(new Output());
+// }
 
 public interface Storage{
   InputReturn input();
