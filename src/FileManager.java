4cfbdd0a311f5d6287f953116be85052283bed8f
Jaycodes13 <157427609+Jaycodes13@users.noreply.github.com>
2024-02-22 20:32:35 -0500

    Implemented Engine methods and UserStart methods. Updated Input and Output implementations. (#18) (#21)
    * Implemented Engine methods and UserStart methods. Updated Input and Output implementations. (#18)
    * Implemented Engine methods and UserStart methods. Updated Input and Output implementations.
    
    * Formatted for checkstyles
    
    * Update FileManager.java
    
    adjusted indentation for checkstyles
    
    * Update WriteReturn.java
    
    adjusted formatting for checkstyles
    
    ---------
    
    Co-authored-by: Shelacula <40150200+Shelacula@users.noreply.github.com>

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,9 +1,14 @@
 package src;
 
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class FileManager implements FileManagerAPI{
 
@@ -31,14 +36,41 @@ public class FileManager implements FileManagerAPI{
   }
 
   @Override
-  public IWriteReturn write(IOutput write, String computed) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'write'");
+  public WriteReturn write(IOutput write, String computed) throws IOException {
+    Output output = (Output) write;
+    String outPath = output.outputPath;
+    File outputFile = new File(outPath);
+    BufferedWriter out = new BufferedWriter(new FileWriter(outputFile));
+    try {
+      out.write(computed + ';');
+      out.close();
+      return new WriteReturn(true);
+    } catch (Exception e) {
+      return new WriteReturn(false);
+    }
   }
+  
+  
 
   @Override
   public IWriteReturn write(IOutput write, String computed, String delim) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'write'");
+    try {
+      Output output = (Output) write;
+      String outPath = output.outputPath;
+      File outputFile = new File(outPath);
+
+      outputFile.getParentFile().mkdirs();
+
+      try (BufferedWriter out = new BufferedWriter(new FileWriter(outputFile))) {
+        out.write(computed + delim);
+        return new WriteReturn(true);
+      } catch (IOException e) {
+        e.printStackTrace();
+        return new WriteReturn(false);
+      }
+    } catch (ClassCastException e) {
+      e.printStackTrace();
+      return new WriteReturn(false);
+    }
   }
 }
